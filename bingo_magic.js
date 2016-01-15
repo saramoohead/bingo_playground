f461a8d61d50d9fe2b54124d3b4ca5164e0943e4
Sara OC <saramoo@hotmail.com>
2016-01-15 18:30:56 +0000

    working with 75

--- bingo_magic.js
+++ bingo_magic.js
@@ -6,7 +6,11 @@ if (Meteor.isClient) {
     Template.next_call.events({
         "click .next_button": function() {
 
-        var array = Images.find().fetch();
+        // ANIMALS
+        var gameCategory = Session.get("gameCategory");
+
+        var array = Images.find({category: gameCategory, exclude: false}).fetch();
+        console.log("array", array.length, array);
         var randomIndex = Math.floor( Math.random() * array.length );
         var currentImageSRC = array[randomIndex];
 
@@ -16,10 +20,53 @@ if (Meteor.isClient) {
             // console.log("Step 2 inside click next", currentImageSRC);
             Session.set("currentImage", currentImageSRC);
             // console.log("Step 3 inside click next", currentImageSRC);
-            return currentImageSRC;
+
+
+        // NUMBERS
+        var N = 76;
+        var bingoNumbers = Array.apply(null, {length: N}).map(Number.call, Number);
+        bingoNumbers.shift();
+        console.log("bingoNumbers", bingoNumbers);
+
+        randomIndex = Math.floor( Math.random() * bingoNumbers.length );
+        var firstNumber = bingoNumbers[randomIndex];
+
+        console.log("firstNumber", firstNumber);
+        var index = bingoNumbers.indexOf(firstNumber);
+        bingoNumbers.splice(index, 1);
+
+
+        randomIndex = Math.floor( Math.random() * bingoNumbers.length );
+        var secondNumber = bingoNumbers[randomIndex];
+
+        console.log("secondNumber", secondNumber);
+        index = bingoNumbers.indexOf(secondNumber);
+        bingoNumbers.splice(index, 1);
+        console.log("bingoNumbersAfter", bingoNumbers);
+
+        Session.set("firstNumber", firstNumber);
+        Session.set("secondNumber", secondNumber);
+
+        Session.set("bingoNumbers", bingoNumbers);
+        console.log(Session.get("bingoNumbers"));
+
+        return currentImageSRC, firstNumber, secondNumber;
         // }
+        }
+    });
 
-
+    Template.new_game.events({
+        "click .new_farm": function() {
+            Meteor.call("resetExcludes");
+            Session.set("gameCategory", "Farm");
+        },
+        "click .new_jungle": function() {
+            Meteor.call("resetExcludes");
+            Session.set("gameCategory", "Jungle");
+        },
+        "click .new_sweets": function() {
+            Meteor.call("resetExcludes");
+            Session.set("gameCategory", "Sweets");
         }
     });
 
@@ -32,6 +79,27 @@ if (Meteor.isClient) {
         }
     });
 
+    Template.numbers_bingo.helpers({
+        firstNumber: function () {
+
+            var firstNumber = Session.get("firstNumber");
+            return firstNumber;
+
+        },
+        secondNumber: function () {
+            var secondNumber = Session.get("secondNumber");
+            return secondNumber;
+        }
+    });
+
+    Template.called_animals.helpers({
+        calledImages: function () {
+            var calledImages = Images.find({ exclude: true });
+            if (calledImages) {
+                return calledImages;
+            }
+        }
+    });
 
 }
 
